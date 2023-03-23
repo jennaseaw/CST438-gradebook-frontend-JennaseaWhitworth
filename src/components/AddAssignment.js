b1ea096959bcb25bb86ad98a0fbabcdd0ac0218c
jennaseaw <jennaseawhitworth@gmail.com>
2023-03-23 11:40:37 -0700

    AddAssignment button working, but not adding assignment to database

--- src/components/AddAssignment.js
+++ src/components/AddAssignment.js
@@ -3,162 +3,102 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Grid from '@mui/material/Grid';
 import {DataGrid} from '@mui/x-data-grid';
-import Button from '@mui/material/Button';
 import Cookies from 'js-cookie';
 import {SERVER_URL} from '../constants.js';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 class AddAssignment extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {selected: 0, assignments: []};
+      this.state = {open:false ,assignment: []};
     };
  
-   componentDidMount() {
-    this.fetchAssignments();
-  }
+   
  
-  fetchAssignments = () => {
-    console.log("Assignment.fetchAssignments");
-    const token = Cookies.get('XSRF-TOKEN');
-    fetch(`${SERVER_URL}/addAssignment`, 
-      {  
-        method: 'GET', 
-        headers: { 'X-XSRF-TOKEN': token }
-      } )
-    .then((response) => response.json()) 
-    .then((responseData) => { 
-      if (Array.isArray(responseData.assignments)) {
-        //  add to each assignment an "id"  This is required by DataGrid  "id" is the row index in the data grid table 
-        this.setState({ assignments: responseData.assignments.map((assignment, index) => ( { id: index, ...assignment } )) });
-      } else {
-        toast.error("Fetch failed.", {
-          position: toast.POSITION.BOTTOM_LEFT
-        });
-      }        
-    })
-    .catch(err => console.error(err)); 
-  }
-  
-   onRadioClick = (event) => {
-    console.log("Assignment.onRadioClick " + event.target.value);
-    this.setState({selected: event.target.value});
-  }
-  
-  handleSubmit = ( ) => {
-      console.log("Assignment.handleSubmit");
-      const token = Cookies.get('XSRF-TOKEN');
-      
-      fetch(`${SERVER_URL}/addAssignment/${this.props.location.assignment.assignmentId}` , 
-          {  
-            method: 'PUT', 
-            headers: { 'Content-Type': 'application/json',
-                       'X-XSRF-TOKEN': token }, 
-            body: JSON.stringify({assignmentId:this.props.location.assignment.assignmentId})
-          } )
-      .then(res => {
-          if (res.ok) {
-            toast.success("Assignment successfully added", {
-            position: toast.POSITION.BOTTOM_LEFT
-            });
-            this.fetchGrades();
-          } else {
-            toast.error("Assignment not added", {
-            position: toast.POSITION.BOTTOM_LEFT
-            });
-            console.error('Put http status =' + res.status);
-      }})
-        .catch(err => {
-          toast.error("Assignment not added", {
-            position: toast.POSITION.BOTTOM_LEFT
-          });
-          console.error(err);
-        });
-   };        
+   
+    handleClickOpen = () => {
+      this.setState( {open:true} );
+    };
+
+   
+   handleClose = () => {
+   	this.setState({open:false});
+   }
+    
+     handleChange = (event) => {
+      this.setState({assignment:{assignment_id: event.target.value}});
+    }
+    
+     handleAdd = () => {
+       this.props.addAssignment(this.state.assignment);
+       this.handleClose();
+    }
     
     // when user has entered a new grade, update the state
     //  id    - index of row of grade change
     //  props - contains the new grade
-    handleEditCellChange = ({ id, field, props }) => {
-       console.log("edit cell change id:"+id+" field:"+field+" value:"+props.value);
-       const newgrades = this.state.grades.map(r => {
-         if (r.id === id){
-           return {...r, [field]:props.value};
-         } else {
-           return {...r};
-         }
-       });
-       this.setState({grades: newgrades});
-     };
-
-    handleCellEditCommit = (e) => {
-      console.log("handleCellEditCommit "+JSON.stringify(e));
-      const newgrades= this.state.grades.map(r => {
-        //console.log(r.id+"   "+e.id);
-        if (r.id === e.id) {
-          return {...r, [e.field]:e.value};
-        } else {
-          return {...r};
-        }
-      });
-      this.setState({grades: newgrades});
-    };
     
-    handleEditCellChange = ({ id, field, props }) => {
-       console.log("edit cell change id:"+id+" field:"+field+" value:"+props.value);
-       const newAssignmentNames = this.state.assignmentNames.map(r => {
-         if (r.id === id){
-           return {...r, [field]:props.value};
-         } else {
-           return {...r};
-         }
-       });
-       this.setState({assignmentNames: newAssignmentNames});
-     };
-
-    handleCellEditCommit = (e) => {
-      console.log("handleCellEditCommit "+JSON.stringify(e));
-      const newAssignmentNames = this.state.assignmentNAMES.map(r => {
-        //console.log(r.id+"   "+e.id);
-        if (r.id === e.id) {
-          return {...r, [e.field]:e.value};
-        } else {
-          return {...r};
-        }
-      });
-      this.setState({assignmentNames: newAssignmentNames});
-    };
+    
+    // handleEditCellChange = ({ id, field, props }) => {
+//        console.log("edit cell change id:"+id+" field:"+field+" value:"+props.value);
+//        const newAssignmentNames = this.state.assignmentNames.map(r => {
+//          if (r.id === id){
+//            return {...r, [field]:props.value};
+//          } else {
+//            return {...r};
+//          }
+//        });
+//        this.setState({assignmentNames: newAssignmentNames});
+//      };
+// 
+//     handleCellEditCommit = (e) => {
+//       console.log("handleCellEditCommit "+JSON.stringify(e));
+//       const newAssignmentNames = this.state.assignmentNAMES.map(r => {
+//         //console.log(r.id+"   "+e.id);
+//         if (r.id === e.id) {
+//           return {...r, [e.field]:e.value};
+//         } else {
+//           return {...r};
+//         }
+//       });
+//       this.setState({assignmentNames: newAssignmentNames});
+//     };
     
        
     render() {
-       const columns = [
-        { field: 'assignmentName', headerName: 'Assignment Name', width: 250, editable:true},
-        { field: 'courseTitle', headerName: 'Course', width: 250, editable:true},
-        { field: 'date', headerName: 'Due Date', width: 150 , editable:true}
-        ];
-        
-        const assignment = this.props.location.assignment;
-      
+       // const columns = [
+//         { field: 'assignmentName', headerName: 'Assignment Name', width: 250, editable:true},
+//         { field: 'courseTitle', headerName: 'Course', width: 250, editable:true},
+//         { field: 'date', headerName: 'Due Date', width: 150 , editable:true}
+//         ];
+//         
+//         const assignment = this.props.location.assignment;
+//       
         return(
-            <div className="App">
-              <Grid container>
-                <Grid item align="left">
-                   <h4>Assignment: {assignment.assignmentName}</h4>
-                   <h4>Course: {assignment.courseTitle}</h4>                   
-                </Grid>
-              </Grid>
-              <div style={{width:'100%'}}>
-                For DEBUG:  display state.
-                {JSON.stringify(this.state)}
-              </div>
-              <div style={{ height: 400, width: '100%' }}>
-                <DataGrid rows={this.state.assignmentNames} columns={columns} onCellEditCommit={this.handleCellEditCommit}  />
-                <DataGrid rows={this.state.grades} columns={columns} onCellEditCommit={this.handleCellEditCommit}  />
-                <Button id="Submit" variant="outlined" color="primary" style={{margin: 10}} onClick={this.handleSubmit} >
-                   Submit
-                </Button>
-              </div>
-              <ToastContainer autoClose={1500} />   
-            </div>
+           <div>
+            <Button variant="outlined" color="primary" style={{margin: 10}} onClick={this.handleClickOpen}>
+              Add Assignment
+            </Button>
+            <Dialog open={this.state.open} onClose={this.handleClose}>
+                <DialogTitle>Add Assignment</DialogTitle>
+                <DialogContent  style={{paddingTop: 20}} >
+                  <TextField autoFocus fullWidth label="Assignment Name" name="name" onChange={this.handleChange}  /> 
+                  <TextField autoFocus fullWidth label="Course Title" name="courseTitle" onChange={this.handleChange}  /> 
+                  <TextField autoFocus fullWidth label="Due Date" name="dueDate" onChange={this.handleChange}  /> 
+                </DialogContent>
+                <DialogActions>
+                  <Button color="secondary" onClick={this.handleClose}>Cancel</Button>
+                  <Button id="Add" color="primary" onClick={this.handleAdd}>Add</Button>
+                </DialogActions>
+              </Dialog>      
+          </div>
             ); 
         };
 }
+
+export default AddAssignment;
--- src/components/Assignment.js
+++ src/components/Assignment.js
@@ -5,14 +5,16 @@ import { Link } from 'react-router-dom'
 import Cookies from 'js-cookie';
 import Button from '@mui/material/Button';
 import Radio from '@mui/material/Radio';
+import Grid from '@mui/material/Grid';
 import {DataGrid} from '@mui/x-data-grid';
 import {SERVER_URL} from '../constants.js';
-import AddAssignment from './AddAssignment'
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
 import TextField from "@mui/material/TextField";
 import DialogActions from "@mui/material/DialogActions";
+import ButtonGroup from '@mui/material/ButtonGroup';
+import AddAssignment from './AddAssignment';
 
 // NOTE:  for OAuth security, http request must have
 //   credentials: 'include' 
@@ -56,37 +58,55 @@ class Assignment extends React.Component {
     this.setState({selected: event.target.value});
   }
   
-  // addAssignment = (assignment) => {
-//     console.log("Adding assignment... name:" + assignment["name"] );
-//     
-//     const token = Cookies.get('XSRF-TOKEN');
-//     fetch(`${SERVER_URL}gradebook`,
-//         {
-//             method: 'POST',
-//             headers: { 'Content-Type': 'application/json',
-//                 'X-XSRF-TOKEN': token  },
-//             body: JSON.stringify(assignment)
-//         })
-//         .then(res => {
-//             if (res.ok) {
-//                 toast.success("Assignment successfully added", {
-//                     position: toast.POSITION.BOTTOM_LEFT
-//                 });
-//                 this.fetchAssignments();
-//             } else {
-//                 toast.error("Error when adding", {
-//                     position: toast.POSITION.BOTTOM_LEFT
-//                 });
-//                 console.error('Post http status =' + res.status);
-//             }})
-//         .catch(err => {
-//             toast.error("Error when adding", {
-//                 position: toast.POSITION.BOTTOM_LEFT
-//             });
-//             console.error(err);
-//         })
-// }
-//   
+  addAssignment = (assignment) => {
+    console.log("Adding assignment... name:" + assignment["name"] );
+    
+    const token = Cookies.get('XSRF-TOKEN');
+    fetch(`${SERVER_URL}gradebook`,
+        {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json',
+                'X-XSRF-TOKEN': token  },
+            body: JSON.stringify(assignment)
+        })
+        .then(res => {
+            if (res.ok) {
+                toast.success("Assignment successfully added", {
+                    position: toast.POSITION.BOTTOM_LEFT
+                });
+                this.fetchAssignments();
+            } else {
+                toast.error("Error when adding", {
+                    position: toast.POSITION.BOTTOM_LEFT
+                });
+                console.error('Post http status =' + res.status);
+            }})
+        .catch(err => {
+            toast.error("Error when adding", {
+                position: toast.POSITION.BOTTOM_LEFT
+            });
+            console.error(err);
+        })
+}
+
+             // <Button component={Link} to={{pathname:'/AddAssignment',   assignment: assignmentSelected }} 
+//                     variant="outlined" color="primary" disabled={this.state.assignments.length===0}  style={{margin: 10}}>
+//               Add 
+//             </Button>
+              // <Button variant="outlined" color="primary" style={{margin: 10}} onClick={this.handleClickOpen}>
+//         		Add Assignment
+//    			 </Button>
+//     		<Dialog open={this.state.open} onClose={this.handleClose}>
+//        		<DialogTitle>Add Assignment</DialogTitle>
+//         	<DialogContent style={{paddingTop: 20}} >
+//             	<TextField fullWidth label="Name" name="assignmentName" onChange={this.handleNameChange}  />
+//             	<TextField fullWidth label="Course" name="courseTitle" onChange={this.handleEmailChange}  />
+//         	</DialogContent>
+//         	<DialogActions>
+//             	<Button color="secondary" onClick={this.handleClose}>Cancel</Button>
+//             	<Button id="Add" color="primary" onClick={this.handleAdd}>Add</Button>
+//         	</DialogActions>
+//    	 		</Dialog>
 	handleSubmit = () => {
    		this.props.addAssignment(this.state.assignment);
 		this.handleClose();
@@ -129,24 +149,13 @@ class Assignment extends React.Component {
                     variant="outlined" color="primary" disabled={this.state.assignments.length===0}  style={{margin: 10}}>
               Grade
             </Button>
-             <Button component={Link} to={{pathname:'/AddAssignment',   assignment: assignmentSelected }} 
-                    variant="outlined" color="primary" disabled={this.state.assignments.length===0}  style={{margin: 10}}>
-              Add 
-            </Button>
-            <Button variant="outlined" color="primary" style={{margin: 10}} onClick={this.handleClickOpen}>
-        		Add Assignment
-   			 </Button>
-    		<Dialog open={this.state.open} onClose={this.handleClose}>
-       		<DialogTitle>Add Assignment</DialogTitle>
-        	<DialogContent style={{paddingTop: 20}} >
-            	<TextField fullWidth label="Name" name="assignmentName" onChange={this.handleNameChange}  />
-            	<TextField fullWidth label="Course" name="courseTitle" onChange={this.handleEmailChange}  />
-        	</DialogContent>
-        	<DialogActions>
-            	<Button color="secondary" onClick={this.handleClose}>Cancel</Button>
-            	<Button id="Add" color="primary" onClick={this.handleAdd}>Add</Button>
-        	</DialogActions>
-   	 		</Dialog>
+   	 		<Grid container>
+              <Grid item>
+			    <ButtonGroup>
+                  <AddAssignment addAssignment={this.addAssignment}  />
+				</ButtonGroup>
+              </Grid>
+            </Grid>
            
             <ToastContainer autoClose={1500} /> 
           </div>
